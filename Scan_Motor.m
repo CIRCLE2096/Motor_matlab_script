4aa161b1588e85925020a3c19edc1e1d23fac54d
liyifei <liyifei12344321@sina.com>
2021-12-29 17:18:51 +0800

    edit Scan_Motor.

--- Scan_Motor.m
+++ Scan_Motor.m
@@ -2,25 +2,24 @@ function Scan_Motor
 %SCAN_MOTOR 此处显示有关此函数的摘要
 %   此处显示详细说明
 persistent x;
-persistent port;
-if isempty(port)
-   port = serial('COM3','BaudRate',9600);
-   fopen(port);
-end
 if isempty(x)
    x=0;
-   set_speed(port,'x', 10);
 end
+
+port = serial('COM3','BaudRate',9600);
+set_speed(port,'x', 10);
 set_position(port, 'x', x);
+set_position(port, 'y', 0);
+set_position(port, 'z', 0);
 start(port,1);
+start(port,0);
 while 1 ~= check_arrive(port)
 end
 if x<50
     x = x+5;
 else
-    fclose(port);
     delete(port);
-    clear port
+    clear port;
 end
 
 end
--- test.m
+++ test.m
@@ -1,12 +1,13 @@
-port = serial('COM4','BaudRate',9600);
+port = serial('COM3','BaudRate',9600);
 try
     set_speed(port,'x', 10);
-    set_position(port, 'x', 50);
-    set_position(port, 'y', 0);
+    set_position(port, 'x', 100);
+    set_position(port, 'y', 10);
     set_position(port, 'z', 0);
     start(port,1);
     start(port,0);
     A = check_arrive(port);
+    fclose(port);
     delete(port);
 catch
     delete(port);
