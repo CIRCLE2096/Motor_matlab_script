7e94005e8de3a98cb6ad53545a02097f91f4a2be
liyifei <liyifei12344321@sina.com>
2021-12-29 16:16:50 +0800

    close port in every function.

--- set_speed.m
+++ set_speed.m
@@ -1,23 +1,29 @@
 function set_speed(port, axis, speed)
-station_num = '01';
-fun_write_D = '10';
-switch axis
-    case 'x'
-        address_position = ['06';'42'];
-    case 'y'
-        address_position = ['06';'46'];
-    case 'z'
-        address_position = ['06';'4A'];
-end
-address_num_2 = ['00';'02'];
-Byte_num_4 = '04';
+try
+    fopen(port);
+    station_num = '01';
+    fun_write_D = '10';
+    switch axis
+        case 'x'
+            address_position = ['06';'42'];
+        case 'y'
+            address_position = ['06';'46'];
+        case 'z'
+            address_position = ['06';'4A'];
+    end
+    address_num_2 = ['00';'02'];
+    Byte_num_4 = '04';
 
-Com_num =[station_num; fun_write_D; address_position; address_num_2; Byte_num_4];
-Com_num = hex_add_single(Com_num,single(speed));
-crc = crc16_modbus(Com_num);
-Com_num = [Com_num; crc];
-disp(Com_num);
-disp('*******');
-Com_num = hex2dec(Com_num);
-fwrite(port,Com_num);
+    Com_num =[station_num; fun_write_D; address_position; address_num_2; Byte_num_4];
+    Com_num = hex_add_single(Com_num,speed);
+    crc = crc16_modbus(Com_num);
+    Com_num = [Com_num; crc];
+    Com_num = hex2dec(Com_num);
+    fwrite(port,Com_num);
+catch e
+    disp(e);
+    fclose(port);
 end
+fclose(port);
+end
+
--- test.m
+++ test.m
@@ -1,7 +1,13 @@
-port = serial('COM3','BaudRate',9600);
-fopen(port);
-set_speed(port,'x', 10);
-set_position(port, 'x', 2);
-start(port,1);
-fclose(port);
+port = serial('COM4','BaudRate',9600);
+try
+    set_speed(port,'x', 10);
+    set_position(port, 'x', 50);
+    start(port,1);
+    A = check_arrive(port);
+    fclose(port);
+    delete(port);
+catch
+    delete(port);
+end
 delete(port);
+
